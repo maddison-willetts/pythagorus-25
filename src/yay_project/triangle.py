1788458aa9a0dcea9545791315afdf0eb2d95437
maddison-willetts <en23mw@leeds.ac.uk>
2025-07-03 11:17:09 +0000

    changed code

---
+++ src/yay_project/triangle.py
@@ -0,0 +1,14 @@
+# Module to calculate hypotenuse of triangle
+
+#Hypotenuse calculation function
+#Takes lengths a and b (opposite and adjacent)
+#of a triangle
+#amd returns c, the hypotenuse
+
+#squaring function
+#takes length a 
+#returns a^2
+
+#adding function
+#takes lengths a and b 
+#returns a + b
